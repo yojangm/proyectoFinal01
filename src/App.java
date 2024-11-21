74b3659d14a669fb4d4c6d4383e00bf6964cca45
Brayan Steven Perez Hernandez <brayanstevenp16@gmail.com>
2024-11-21 15:50:47 -0500

    Seleccionar nave

--- src/App.java
+++ src/App.java
@@ -7,15 +7,16 @@ public class App {
     static String[] planetas = {"Marte", "Jupiter", "Saturno"};
     static double[] distancias = {78.0, 628.0, 1256.0}; 
 
-    static String[] naves = {};
+    static String[] naves = {"Pelican", "Atlantic", "Halcón Milenario"};
     static double[] velocidades = {};
 
     public static void main(String[] args) throws Exception {
         
         int option;
+        mostrarMenu();
         do {
             
-            mostrarMenu();
+         
             option = scanner.nextInt();
             switch (option) {
                 case 1:
@@ -53,9 +54,25 @@ public class App {
         System.out.println("\n ---Menú Naves---");
         System.out.println("1. Pelican.");
         System.out.println("2. Atlantic.");
-        System.out.println("3. Estrella de la muerte.");
-        System.out.println("4. Halcon Milenatio");
-        System.out.println("5. Salir.");
+        System.out.println("3. Halcón Milenario");
+        System.out.println("4. Volver al menú principal.");
+
+        System.out.println("Seleccione la nave");
+        int option = scanner.nextInt();
+
+        switch (option) {
+            case 1:
+                System.out.printf("La nave seleccionada es " + naves[0]);
+                break;
+            case 2:
+                System.out.println("La nave seleccionada es " + naves[1]);
+                break;
+            case 3:
+                System.out.println("La nave seleccionada es " + naves[2]);
+                break;
+            default:
+                break;
+        }
 
     }
 
